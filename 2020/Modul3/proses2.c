3d008e0827a5c8e0049f38361fc416d8ba8609e3
leleterbank <93sultanbaney@gmail.com>
2020-07-29 03:32:45 +0700

    Modul 3 + 4

---
+++ 2020/Modul3/proses2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+void main()
+{
+        key_t key = 1234;
+        int *value;
+
+        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+        value = shmat(shmid, NULL, 0);
+
+        printf("Program 1 : %d\n", *value);
+	*value = 30;
+
+        sleep(5);
+
+        printf("Program 1: %d\n", *value);
+        shmdt(value);
+        shmctl(shmid, IPC_RMID, NULL);
+}
