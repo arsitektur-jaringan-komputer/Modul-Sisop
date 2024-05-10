368f7b5dccff222c60353db67632c9663c485de6
mvinorian <vinorianmuhammad@gmail.com>
2024-05-10 19:02:46 +0700

    docs: add modul4 osdev ID

---
+++ Modul4/playground/bootloader.asm
@@ -0,0 +1,41 @@
+; bootloader.asm
+bits 16
+
+KERNEL_SEGMENT equ 0x1000 ; kernel will be loaded at 0x1000:0x0000
+KERNEL_SECTORS equ 15     ; kernel will be loaded in 15 sectors maximum
+KERNEL_START   equ 1      ; kernel will be loaded in sector 1
+
+; bootloader code
+bootloader:
+  ; load kernel to memory
+  mov ax, KERNEL_SEGMENT    ; load address of kernel
+  mov es, ax                ; buffer address are in ES:BX
+  mov bx, 0x0000            ; set buffer address to KERNEL_SEGMENT:0x0000
+
+  mov ah, 0x02              ; read disk sectors
+  mov al, KERNEL_SECTORS    ; number of sectors to read
+
+  mov ch, 0x00              ; cylinder number
+  mov cl, KERNEL_START + 1  ; sector number
+  mov dh, 0x00              ; head number
+  mov dl, 0x00              ; read from drive A
+
+  int 0x13                  ; call BIOS interrupts
+
+  ; set up segment registers
+  mov ax, KERNEL_SEGMENT
+  mov ds, ax
+  mov es, ax
+  mov ss, ax
+
+  ; set up stack pointer
+  mov ax, 0xFFFF
+  mov sp, ax
+  mov bp, ax
+
+  ; jump to kernel
+  jmp KERNEL_SEGMENT:0x0000
+
+  ; padding to make bootloader 512 bytes
+  times 510-($-$$) db 0
+  dw 0xAA55
